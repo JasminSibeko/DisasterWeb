183137cce8af1275f2ee0b9effb2154eb954f04e
RC_Student_Lab <RC_Student_Lab@LabVM1846780>
2025-10-03 12:58:28 +0200

    Add project files.

---
+++ DisasterWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DisasterWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
